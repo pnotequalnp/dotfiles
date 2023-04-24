d898c26c17f28ae1de74eeac901ae9fd89d9cb6e
Kevin Mullins <kevin@pnotequalnp.com>
2023-04-24 13:04:40 -0700

    Add basic wofi configuration

--- user/desktop/wofi.nix
+++ user/desktop/wofi.nix
@@ -1,5 +1,43 @@
-{ pkgs, ... }:
+{ config, ... }:
 
-{
-  home.packages = with pkgs; [ wofi ];
+let
+  inherit (config.colorScheme) colors;
+  hash = color: "#${color}";
+in {
+  programs.wofi = {
+    enable = true;
+    settings = {
+      show = "drun";
+      insensitive = true;
+      allow_images = true;
+      allow_markup = true;
+      no_actions = true;
+      prompt = "";
+      key_up = "Control_L-p";
+      key_down = "Control_L-n";
+    };
+
+    style = ''
+      * {
+        font-family: Iosevka;
+      }
+  
+      #window {
+        background-color: ${hash colors.base01};
+      }
+
+      #input {
+        background-color: ${hash colors.base01};
+        color: ${hash colors.base05}
+      }
+
+      #text {
+        color: ${hash colors.base05};
+      }
+
+      #entry:selected {
+        background-color: ${hash colors.base02};
+      }
+  '';
+  };
 }
